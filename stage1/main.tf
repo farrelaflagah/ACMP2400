996ff694a54b79e83104037bda26b3285cecb300
farrelaflagah <yaofarrel@gmail.com>
2026-04-28 00:36:13 -0500

    Add Terraform Stage 1 main.tf

---
+++ stage1/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name       = "big-tf-state-acmp2400"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "faflagah-acr" {
+  name                = "acrfaflagahacmp2400"
+  resource_group_name = "rg-faflagah"
+  location            = "Central US"
+  sku                 = "Basic"
+  admin_enabled       = false
+}
