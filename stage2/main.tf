7e84efcbb0966eda91a27efcf0f6a9d07dd42587
farrelaflagah <yaofarrel@gmail.com>
2026-04-30 00:15:07 -0500

    Add Terraform Stage 2 to workflow

---
+++ stage2/main.tf
@@ -0,0 +1,62 @@
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name       = "big-tf-state-acmp2400"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "faflagah-acr" {
+  name                = "acrfaflagahacmp2400"
+  resource_group_name = "rg-faflagah"
+  location            = "Central US"
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_container_group" "aci-faflagah-acmp" {
+  name                = "aci-faflagah-acmp"
+  location            = "Central US"
+  resource_group_name = "rg-faflagah"
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-faflagah-acmp"
+  os_type             = "Linux"
+
+  container {
+    name   = "final-app"
+    image  = "acrfaflagahacmp2400.azurecr.io/final:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 8000
+      protocol = "TCP"
+    }
+
+    secure_environment_variables {
+      DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+    }
+  }
+
+  image_registry_credential {
+    server   = "acrfaflagahacmp2400.azurecr.io"
+    username = var.ARM_CLIENT_ID
+    password = var.ARM_CLIENT_SECRET
+  }
+}
